85f73c4c836182cf8724ce00ce83a8e1132a1e73
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-23 02:28:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,12 @@ def roll_call_dwarves(array)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.map.with_index { |phrase, index| 
+    new_array[index] = phrase.capitalize + "!"
+  }
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
